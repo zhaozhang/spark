cad17dd45a3e30557a22f9dbed0f073e3e1c2e88
Zhao Zhang <zhangzhao0718@gmail.com>
2016-02-17 15:21:27 -0800

    one more try

--- core/src/main/scala/org/apache/spark/rdd/NewHadoopRDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/NewHadoopRDD.scala
@@ -240,6 +240,7 @@ class NewHadoopRDD[K, V](
         }
       case None => None
     }
+    locs.getOrElse(println(split.getLocations.toString))
     locs.getOrElse(Random.shuffle(split.getLocations.filter(_ != "localhost")))
   }
 
